0be1209fe14ff48650b8bd8cd951bda6e382dfa1
Jonathan javier Urquiza <114363507+JonathanUrquiza@users.noreply.github.com>
2025-02-07 15:31:01 -0300

    [FEAT] Correcciones

--- js/index.js
+++ js/index.js
@@ -1,24 +1,98 @@
 const btnArray = document.querySelectorAll('.btn-option');
+const btnsMenu = document.querySelectorAll('.btn');
+const btnMobile =document.querySelector('.btn-menu');
+const btnClose = document.querySelector('.btn-close');
 const ziseContainer = document.querySelector('.size-container');
 const carrousel = document.querySelector('.carrouse');
-const marcador = document.querySelectorAll('.marcador')
-
+const marcador = document.querySelectorAll('.marcador');
+const menu = document.querySelector('.menu');
+const tiendaMenu = document.getElementById('shop-submenu'),
+ collections = document.getElementById('collection-submenu'),
+ arrivals = document.getElementById('arrivals-submenu'),
+ aboutUs = document.getElementById('about-submenu');
+let condition = "open";
 //-------------------------------------------------------------//
 
-
 //cuando hace click en algun botón
-    //cambia la clase del elemento para mostrarlo en pantalla
+//cambia la clase del elemento para mostrarlo en pantalla
 btnArray.forEach((btnShop) => {
     btnShop.addEventListener('click', () => {
         ziseContainer.classList.toggle('active')
     })
 })
+
+
+btnMobile.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
+btnClose.addEventListener('click', () => {
+    menu.classList.add('ocultar');
+});
+
+
+
+
+
+
+btnsMenu[0].addEventListener('click', () => {
+    const tienda = tiendaMenu.classList
+    if (tienda.value == 'ocultar') {
+        tiendaMenu.classList.remove('ocultar');
+        tiendaMenu.classList.add('shop-submenu')
+        condition = 'close';
+        console.log(condition);
+    } else {
+        tiendaMenu.classList.remove('shop-submenu')
+        tiendaMenu.classList.add('ocultar');
+        condition = 'open';
+        console.log(condition);
+    }
+})
+
+btnsMenu[1].addEventListener('click', () => {
+    const collections = collections.classList
+    if (condition == "open") {
+       
+        console.log(condition);
+        condition = 'close';
+
+
+    } else {
+        console.log(condition);
+        condition = 'open';
+    }
+})
+
+
+btnsMenu.forEach((btn, ) => {
+    btn.addEventListener('click', () => {
+        if (condition == "open") {
+            console.log(condition);
+            condition = 'close';
+    
+    
+        } else {
+            console.log(condition);
+            condition = 'open';
+        }
+
+    })
+
+})
+
+
+
+
+
+
+//5968725
+
 //-------------------------------------------------------------//
 // Cuando CLICK en punto
-    // Saber la posición de ese punto
-    // Aplicar un transform translateX al grande
-    // QUITAR la clase activo de TODOS puntos
-    // AÑADIR la clase activo al punto que hemos hecho CLICK
+// Saber la posición de ese punto
+// Aplicar un transform translateX al grande
+// QUITAR la clase activo de TODOS puntos
+// AÑADIR la clase activo al punto que hemos hecho CLICK
 
 ////Recorre todos los marcadores
 //    marcador.forEach((marca, i) => {
@@ -28,7 +102,7 @@ btnArray.forEach((btnShop) => {
 //            let position = i
 //            //calcula el espacio que debe desplazarse el carrousel
 //            let operation = position * -33.3
-//            //mueve el carrpusel 
+//            //mueve el carrpusel
 //            carrousel.style.transform = `translateX(${operation}%)`
 //
 //            marcador.forEach((marca, i) => {
