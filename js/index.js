114f356e7c338aef53f9dd00ddce673113b8eb9d
Jonathan javier Urquiza <114363507+JonathanUrquiza@users.noreply.github.com>
2025-02-10 12:49:12 -0300

    [FEAT] Programación en js de los botones

--- js/index.js
+++ js/index.js
@@ -1,19 +1,16 @@
-const accountAccess = document.getElementById('acount-access');
 const btnArray = document.querySelectorAll('.btn-option');
 const btnsMenu = document.querySelectorAll('.btn');
-const btnMobile =document.querySelector('.btn-menu');
-const btnClose = document.querySelector('.btn-close');
+
 const ziseContainer = document.querySelector('.size-container');
 const footerContainer = document.querySelector('footer-container');
 const carrousel = document.querySelector('.carrouse');
 const marcador = document.querySelectorAll('.marcador');
-const menu = document.querySelector('.menu');
-const tiendaMenu = document.getElementById('shop-submenu'),
- collections = document.getElementById('collection-submenu'),
- arrivals = document.getElementById('arrivals-submenu'),
- aboutUs = document.getElementById('about-submenu');
- 
-let condition = "open";
+
+
+const collections = document.getElementById('collection-submenu')
+const arrivals = document.getElementById('arrivals-submenu')
+const aboutUs = document.getElementById('about-submenu');
+
 //-------------------------------------------------------------//
 
 //cuando hace click en algun botón
@@ -22,105 +19,63 @@ btnArray.forEach((btnShop) => {
     btnShop.addEventListener('click', () => {
         ziseContainer.classList.toggle('active')
     })
-    
-})
-ziseContainer.addEventListener('click', ()=> {
+});
+ziseContainer.addEventListener('click', () => {
     ziseContainer.classList.toggle('active')
-
-})
-
-btnMobile.addEventListener('click', () => {
-    menu.classList.toggle('active');
 });
-btnClose.addEventListener('click', () => {
-    menu.classList.add('ocultar');
-});
+//Abre el menu hamburguesa
+const btnMenuResponsive = () => {
+    const btnMobile = document.querySelector('.btn-menu');
+    const btnClose = document.querySelector('.btn-close');
+    const menu = document.querySelector('.menu');
+    let state;
+    btnMobile.addEventListener('click', () => {
+        state == undefined? menu.classList.toggle('active'):null
+        state == menu.classList[1]
+    });
+    btnClose.addEventListener('click', () => {
+        state == 'active'? menu.classList.toggle('ocultar'): null
+    });
+}
+//-------------------------------------//
 
-accountAccess.addEventListener('click', () => {
-    const login = document.getElementById('login')
-    const app = document.getElementById('app')
-    app.innerHTML(login)
-})
-
-
-
-btnsMenu[0].addEventListener('click', () => {
-    const tienda = tiendaMenu.classList
-    if (tienda.value == 'ocultar') {
-        tiendaMenu.classList.remove('ocultar');
-        tiendaMenu.classList.add('shop-submenu')
-        condition = 'close';
-        console.log(condition);
-    } else {
-        tiendaMenu.classList.remove('shop-submenu')
-        tiendaMenu.classList.add('ocultar');
-        condition = 'open';
-        console.log(condition);
-    }
-})
-
-btnsMenu[1].addEventListener('click', () => {
-    const collections = collections.classList
-    if (condition == "open") {
-       
-        console.log(condition);
-        condition = 'close';
-
-
-    } else {
-        console.log(condition);
-        condition = 'open';
-    }
-})
-
-
-btnsMenu.forEach((btn, ) => {
-    btn.addEventListener('click', () => {
-        if (condition == "open") {
-            console.log(condition);
-            condition = 'close';
-    
-    
-        } else {
-            console.log(condition);
-            condition = 'open';
-        }
-
+//Boton que desplega el login
+const menuLogin = () => {
+    const login = document.getElementById('login');
+    const accountAccess = document.getElementById('btn-acount-access');
+    const state = login.classList
+    return accountAccess.addEventListener('click', () => {
+        state.value == 'login-off'?login.classList.replace('login-off','login-on'):login.classList.replace('login-on','login-off');
     })
+}
+//-------------------------------------//
+//bonton de apertura de menu
+const btnMenu = () => {
+    const tiendaMenu = document.getElementById('shop-submenu')
+    const tienda = tiendaMenu.classList
+    return btnsMenu[0].addEventListener('click', () => {
+        tienda.value == 'ocultar'?tiendaMenu.classList.replace('ocultar','shop-submenu'):tiendaMenu.classList.replace('shop-submenu','ocultar')
+    })
+}
+//-------------------------------------//
 
-})
+// BOTON DE BUSQUEDA DE ITEMS
+const btnsearch = () => {
+    const btnSearch = document.getElementById('search')
+    const menuSearch = document.getElementById('menusearch');
+    const state = menuSearch.classList
+    return btnSearch.addEventListener('click', () => {
+        state.value == 'hidden'? menuSearch.classList.replace('hidden','show'): menuSearch.classList.replace('show', 'hidden')
+    })
+};
+//-------------------------------------//
+
+//Ejecucion de todas las funciones
+btnMenu();
+menuLogin();
+btnsearch();
+btnMenuResponsive();
 
 
 
-
-
-
-//5968725
-
-//-------------------------------------------------------------//
-// Cuando CLICK en punto
-// Saber la posición de ese punto
-// Aplicar un transform translateX al grande
-// QUITAR la clase activo de TODOS puntos
-// AÑADIR la clase activo al punto que hemos hecho CLICK
-
-////Recorre todos los marcadores
-//    marcador.forEach((marca, i) => {
-//        //asigna un click a cada punto
-//        marca[i].addEventListener('click', () => {
-//            //guarda la posición
-//            let position = i
-//            //calcula el espacio que debe desplazarse el carrousel
-//            let operation = position * -33.3
-//            //mueve el carrpusel
-//            carrousel.style.transform = `translateX(${operation}%)`
-//
-//            marcador.forEach((marca, i) => {
-//                //saca las clases active
-//                marca[i].classList.remove('activo')
-//            })
-//            marca[i].classList('activo')
-//
-//        })
-//})
-//
+//-------------------------------------//
