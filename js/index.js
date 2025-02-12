c4ce39c9cc835b931b1939d63e762f73d6895aee
Jonathan javier Urquiza <114363507+JonathanUrquiza@users.noreply.github.com>
2025-02-12 13:05:44 -0300

    [FEAT] menu mobile

--- js/index.js
+++ js/index.js
@@ -73,14 +73,16 @@ const menuCart = () => {
     const cart = document.getElementById('cart')
      btnCart = document.getElementById('btn-cart'),
      menuSearch = document.getElementById('menusearch'),
-     login = document.getElementById('login')
+     login = document.getElementById('login'),
+     navigation = document.getElementById('navigation')
+     const state = cart.classList
      
-    const state = cart.classList
-    return btnCart.addEventListener('click', () => {
-           state == 'hidden'? cart.classList.replace('hidden','show'):cart.classList.replace('show','hidden')
-           login.classList == 'login-on'? login.classList.replace('login-on', 'login-off'):null;
-           menuSearch.classList == 'show'? menuSearch.classList.replace('show','hidden'):null;
-           ;
+     return btnCart.addEventListener('click', () => {
+         
+         state == 'hidden'? cart.classList.replace('hidden','show'):cart.classList.replace('show','hidden')
+         state != 'hidden'? navigation.classList.replace('navigation','hidden'):null;
+          login.classList == 'login-on'? login.classList.replace('login-on', 'login-off'):null;
+            menuSearch.classList == 'show'? menuSearch.classList.replace('show','hidden'):null;   
     })
 }
 
@@ -88,9 +90,9 @@ const CloseCart = () => {
     const btnCartClose = document.getElementById('close-Cart'),
      cart = document.getElementById('cart')
     const state = cart.classList
-    console.log(cart);
     btnCartClose.addEventListener('click', () => {
         state != 'hidden'?cart.classList.replace('show','hidden'):null
+        state == 'hidden'? navigation.classList.replace('hidden','navigation'):null;
     })
 }
 
